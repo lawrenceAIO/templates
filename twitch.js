d47de545719e44eacc20d0663071877c6622f56f
lawrenceue <lawrence.ue@gmail.com>
2015-08-07 20:12:55 +0000

    http must be included in API hit

--- twitch.js
+++ twitch.js
@@ -1,17 +1,15 @@
-var app = angular.module('test',[]);
+var app = angular.module('test', []);
 
-function TestController($scope, $http){
-  $scope.areaOn = 1;
-  $scope.setArea = function(num){
-    $scope.areaOn = num;
-  } // - end - setArea
-  $scope.thisOn = function(num){
-    return num === $scope.areaOn;
-  } // - end - thisOn
-  
-  $http.get().then(function(dataResp){
-    
-  }) // - end - http - get - Twitch
-  
-} // - end - TestController
-app.controller('TestController',TestController);
+function TestController($scope, $http) {
+    $scope.areaOn = 1;
+    $scope.setArea = function(num) {
+        $scope.areaOn = num;
+      } // - end - setArea
+    $scope.thisOn = function(num) {
+        return num === $scope.areaOn;
+      } // - end - thisOn
+    $http.get("https://api.twitch.tv/kraken/streams/freecodecamp").success(function(response) {
+      $scope.stream = response;
+    }); // - end - http - get - Twitch/dataResp
+
+  } // - end - TestController
